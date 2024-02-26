ccea17594fd1e16eaec4752b45f5777949291339
jsscmnhn <J.A.Monahan@student.tudelft.nl>
2024-02-26 17:57:20 +0100

    linear regression closed form

--- file.py
+++ file.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import statistics as stat
 
 df = pd.read_csv('data/positions.csv')
 
@@ -20,10 +21,10 @@ ax.set_title('Trajectory Plot')
 plt.show()
 
 
-# 2.2a assume constant speed
+# 2.2a assume constant speed linear regression with gradient descent
 # if speed is constant then a * t^2 = 0 therefore
 # f(t) = ax_0 + ax_1 * t
-# objective function/loss is ordinary squared error  = 1 / n (sum (xi - (ax_0 + ax_1 * ti))^2
+# objective function/loss is ordinary squared error  = (sum (xi - (ax_0 + ax_1 * ti))^2
 
 def coefficient_finder(y, t, learning_rate, iterations):
     a0 = 0
@@ -39,7 +40,6 @@ def coefficient_finder(y, t, learning_rate, iterations):
         a0 = a0 - learning_rate * D_a0  # update a0
 
     return a0, a1
-
 
 def predict_speed(a0, a1, t):
     return a0 + a1 * t
@@ -58,8 +58,37 @@ error_z_speed = sum((z - predict_speed(az_0, az_1, t))**2)
 
 
 print('ax_0 = ', ax_0, ' ax_1 = ', ax_1, 'ay_0 = ', ay_0, 'ay_1 = ', ay_1, 'az_0 = ', az_0, 'az_1 = ', az_1)
-print('velocity in x-direction = ',ax_1, '\nvelocity in y-direction =',ay_1, '\nvelocity in z-direction', az_1,)
-print(error_x_speed, error_y_speed, error_z_speed)
+print('velocity in x-direction = ',ax_1, '\nvelocity in y-direction =',ay_1, '\nvelocity in z-direction = ', az_1,)
+print('errors x y z: ', error_x_speed, error_y_speed, error_z_speed)
+
+# 2.2a assume constant speed linear regression with polynomial regression
+# if speed is constant then a * t^2 = 0 therefore
+# f(t) = ax_0 + ax_1 * t
+# objective function/loss is ordinary squared error  = (sum (xi - (ax_0 + ax_1 * ti))^2
+
+def calculate_coefficients_linear(t, y):
+    cov_ty = np.cov(t, y, ddof=1)[0][1]
+    var_t = np.var(t, ddof=1, dtype=float)
+    a1 = cov_ty / var_t
+    a0 = np.mean(y) - a1 * np.mean(t)
+    # print(np.cov(t, y, bias=True), 'cov', cov_ty, 'var_t', var_t, 'a0', a0, 'a1', a1, 'mean', np.mean(t))
+    return a0, a1
+
+ax_0_lin, ax_1_lin = calculate_coefficients_linear(t, x)
+ay_0_lin, ay_1_lin = calculate_coefficients_linear(t, y)
+az_0_lin, az_1_lin = calculate_coefficients_linear(t, z)
+
+error_x_speed_lin = sum((x - predict_speed(ax_0_lin, ax_1_lin, t))**2)
+error_y_speed_lin = sum((y - predict_speed(ay_0_lin, ay_1_lin, t))**2)
+error_z_speed_lin = sum((z - predict_speed(az_0_lin, az_1_lin, t))**2)
+
+print('speed constant linear')
+print('ax_0 = ', ax_0_lin, ' ax_1 = ', ax_1_lin, 'ay_0 = ', ay_0_lin, 'ay_1 = ', ay_1_lin, 'az_0 = ', az_0_lin, 'az_1 = ', az_1_lin)
+print('velocity in x-direction = ',ax_1_lin, '\nvelocity in y-direction =',ay_1_lin, '\nvelocity in z-direction = ', az_1_lin,)
+print('errors x y z: ', error_x_speed_lin, error_y_speed_lin, error_z_speed_lin)
+
+
+
 
 # 2.2b assume constant acceleration
 
@@ -75,17 +104,20 @@ for ax in axs:
     ax.axhline(y=0, color='grey', linestyle='-', linewidth=0.5)
 
 axs[0].scatter(t, x, label='Position of X', color='blue', marker='o')
-axs[0].plot([min(t), max(t)], [predict_speed(ax_0, ax_1, -0.1), predict_speed(ax_0, ax_1, 6.1)], color='red')
+axs[0].plot([min(t), max(t)], [predict_speed(ax_0, ax_1_lin, -0.1), predict_speed(ax_0, ax_1_lin, 6.1)], color='red')
+axs[0].plot([min(t), max(t)], [predict_speed(ax_0_lin, ax_1_lin, -0.1), predict_speed(ax_0_lin, ax_1_lin, 6.1)], color='orange')
 axs[0].set_xlabel('time')
 axs[0].set_ylabel('x_position')
 
 axs[1].scatter(t, y, label='Position of Y', color='red', marker='o')
 axs[1].plot([min(t), max(t)], [predict_speed(ay_0, ay_1, -0.1), predict_speed(ay_0, ay_1, 6.1)], color='red')
+axs[1].plot([min(t), max(t)], [predict_speed(ay_0_lin, ay_1_lin, -0.1), predict_speed(ay_0_lin, ay_1_lin, 6.1)], color='orange')
 axs[1].set_xlabel('time')
 axs[1].set_ylabel('y_position')
 
 axs[2].scatter(t, z, label='Position of Z', color='green', marker='o')
 axs[2].plot([min(t), max(t)], [predict_speed(az_0, az_1, -0.1), predict_speed(az_0, az_1, 6.1)], color='red')
+axs[2].plot([min(t), max(t)], [predict_speed(az_0_lin, az_1_lin, -0.1), predict_speed(az_0_lin, az_1_lin, 6.1)], color='orange')
 axs[2].set_xlabel('time')
 axs[2].set_ylabel('z_position')
 
